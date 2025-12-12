a40286ae3b5e8d2229e6eb23480a1e0f4cd9237c
micheal-ndoh <michaelndoh9@gmail.com>
2025-12-12 23:04:23 +0100

    docs(api): add comments for physical phone API usage

--- mobile/constants/api.js
+++ mobile/constants/api.js
@@ -1,2 +1,4 @@
-// Deployed backend URL for production
+// if you're using your physical phone, change this to the deployed url
+// we have explained this in the course :-)
+
 export const API_URL = "https://recipe-book-lake-delta.vercel.app/api";
