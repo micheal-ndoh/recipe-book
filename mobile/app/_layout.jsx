e72ce3bbed24956d5cf536b118f4a6e7923b901d
micheal-ndoh <michaelndoh9@gmail.com>
2025-12-14 19:16:33 +0100

    Configure deep linking for OAuth callback

--- mobile/app/_layout.jsx
+++ mobile/app/_layout.jsx
@@ -2,13 +2,21 @@ import { Slot } from "expo-router";
 import { ClerkProvider } from "@clerk/clerk-expo";
 import { tokenCache } from "@clerk/clerk-expo/token-cache";
 import * as WebBrowser from "expo-web-browser";
+import Constants from "expo-constants";
 import SafeScreen from "@/components/SafeScreen";
 
 WebBrowser.maybeCompleteAuthSession();
 
+const clerkPublishableKey = Constants.expoConfig.extra.clerkPublishableKey;
+
 export default function RootLayout() {
   return (
-    <ClerkProvider tokenCache={tokenCache}>
+    <ClerkProvider
+      publishableKey={clerkPublishableKey}
+      tokenCache={tokenCache}
+      afterSignInUrl="/"
+      afterSignUpUrl="/"
+    >
       <SafeScreen>
         <Slot />
       </SafeScreen>
